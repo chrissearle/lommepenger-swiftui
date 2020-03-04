c3421b5cf664b9ea03be57d5de25762638b3357b
Chris Searle <chris@chrissearle.org>
2020-03-04 08:49:15 +0100

    Persist

--- Lommepenger/Config.swift
+++ Lommepenger/Config.swift
@@ -1,4 +1,5 @@
 import Foundation
+import KeychainSwift
 
 class Config : Codable {
     let clientId: String
@@ -11,5 +12,45 @@ class Config : Codable {
         case clientSecret
         case userId
         case accountNr
+    }
+}
+
+extension Config {
+    static func decodeConfig(json: String) -> Config? {
+        if let data = json.data(using: .utf8) {
+            let decoder = JSONDecoder()
+
+            if let config = try? decoder.decode(Config.self, from: data) {
+                return config
+            } else {
+                print("Could not decode \(json)")
+            }
+        } else {
+            print("Could not convert to data \(json)")
+        }
+        
+        return nil
+    }
+    
+    func save() {
+        let keychain = KeychainSwift()
+        
+        let encoder = JSONEncoder()
+        
+        if let data = try? encoder.encode(self) {
+            if let json = String(data: data, encoding: .utf8) {
+                keychain.set(json, forKey: "AppConfig")
+            }
+        }
+    }
+    
+    static func loadConfig() -> Config? {
+        let keychain = KeychainSwift()
+        
+        if let json = keychain.get("AppConfig") {
+            return decodeConfig(json: json)
+        }
+        
+        return nil
     }
 }
--- Lommepenger/ContentView.swift
+++ Lommepenger/ContentView.swift
@@ -3,37 +3,39 @@ import CodeScanner
 
 struct ContentView: View {
     @State private var showingScanner = false
+    @State private var config : Config? = nil
     
     var body: some View {
-        Button(action: {
-            self.showingScanner = true
-        }) {
-            Image(systemName: "qrcode")
-                .resizable()
-                .frame(width: 32, height: 32)
+        VStack {
+            Button(action: {
+                self.showingScanner = true
+            }) {
+                Image(systemName: "qrcode")
+                    .resizable()
+                    .frame(width: 32, height: 32)
+            }
+            .sheet(isPresented: $showingScanner) {
+                ScannerView(scannedData: Binding(
+                get: { "" },
+                set: self.newScanData
+                ))
+            }
+
+            if (self.config != nil) {
+                Text(config!.accountNr)
+            } else {
+                Text("You need to scan in a configuation")
+            }
         }
-        .sheet(isPresented: $showingScanner) {
-            ScannerView(scannedData: Binding(
-            get: { "" },
-            set: self.newScanData
-            ))
+        .onAppear {
+            self.config = Config.loadConfig()
         }
     }
     
     func newScanData(_ code: String) {
-        if let data = code.data(using: .utf8) {
-            let decoder = JSONDecoder()
-
-            if let config = try? decoder.decode(Config.self, from: data) {
-                print(config.clientId)
-                print(config.clientSecret)
-                print(config.userId)
-                print(config.accountNr)
-            } else {
-                print("Could not decode \(code)")
-            }
-        } else {
-            print("Could not convert to data \(code)")
+        if let config = Config.decodeConfig(json: code) {
+            config.save()
+            self.config = config
         }
     }
 }
